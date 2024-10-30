6b686113c62a16b493e0926e9eba28653305e4cb
DIVYANSHU KANSWAL <kanswaldivyanshu@gmail.com>
2024-10-30 11:51:25 +0530

    Refactor gitignore to ignore .env file in the api folder

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const fs = require('fs');
+const multer = require('multer');
 
 require('dotenv').config();
 
@@ -14,11 +17,6 @@ const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
 
 const jwtSecret = process.env.JWT_SECRET;
-
-
-
-// const jwtSecret = 'xfa5shiwidbjkss8dw629jsakdk';
-
 
 //middleware
 app.use(express.json());
@@ -29,6 +27,8 @@ app.use(cors({
     origin: 'http://localhost:5173'
    
 }));
+//to display uploaded photo link
+app.use('/uploads',express.static(__dirname+'/uploads'));
 
 mongoose.connect(process.env.MONGO_URL);
 app.get('/test',(req,res)=>{
@@ -121,6 +121,36 @@ app.get('/profile', (req,res)=>{
 app.post('/logout',(req,res)=>{
    res.cookie('token','').json(true);
 
+});
+console.log(__dirname);
+app.post('/upload-by-link',async (req,res)=>{
+    const {link} = req.body;
+    const newName = 'photo' + Date.now()+'.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname+'/uploads/'+newName,
+    })
+    res.json(newName);
+    
+})
+
+//why we use multer
+//multer is used to upload files
+//photosMiddleware.array('photos',100)
+//photos is the name of the field in the form and 100 is the maximum number of files that can be uploaded
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload',photosMiddleware.array('photos',100),(req,res)=>{
+    const uploadedFiles =[];
+    for(let i = 0;i<req.files.length;i++){
+        //rename the file
+        const{path,originalname}=req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length-1];
+        const newPath = path+'.'+ext;
+        fs.renameSync(path,newPath);
+        uploadedFiles.push(newPath.replace('uploads\\',''));
+    }
+    res.json(uploadedFiles);
 });
 
 app.listen(4000);
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { Form, Link, useParams } from "react-router-dom"
 import Perks from "../Perks";
+import axios from 'axios';
+import PhotosUploader from "../PhotosUploader";
+
 
 
 export default function PlacesPage() {
     const { action } = useParams();
     const [title, setTitle] = useState('');
     const [address, setAddress] = useState('');
-    const [addedPhotos, setAddedPhotos] = useState([]);
-    const [photoLink, setPhotoLink] = useState('');
     const [description, setDescription] = useState('');
     const [perks, setPerks] = useState([]);
+    const [addedPhotos, setAddedPhotos] = useState([]);
     const [extraInfo, setExtraInfo] = useState('');
     const [checkInTime, setCheckInTime] = useState('');
     const [checkOutTime, setCheckOutTime] = useState('');
@@ -36,6 +38,7 @@ export default function PlacesPage() {
             </div>
         )
     }
+
     return (
         <div>
             {action !== 'new' && (
@@ -61,19 +64,8 @@ export default function PlacesPage() {
 
 
                             {preInput('Photos', 'Upload photos of your place , better to upload 3-4 photos')}
-                            <div className="flex gap-2">
-                                <input type='text' value={photoLink} onChange={e => setPhotoLink(e.target.value)} placeholder="Photo URL" />
-                                <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
-                            </div>
-                            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                                <button className="flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
-                                    </svg>
-                                    Upload
-                                </button>
-
-                            </div>
+                            <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+                           
                             {preInput('Description', 'Describe your place in detail')}
                             <textarea value={description} onChange={e => setDescription(e.target.value)} />
 
